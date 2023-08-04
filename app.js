28dd497daa2ef752efe7d8d1d7c1a9ec54484e5f
Mayank Thakur <53209069+mayankist@users.noreply.github.com>
2023-08-04 23:43:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json());
 // POST request handler for your API endpoint
 app.post('/api/data', (req, res) => {
   const dataArray = req.body.data;
-  
+
   // Process the dataArray as needed
   console.log('Received data:', dataArray);
-  
+
   // Respond with a success message
   res.json({ message: 'Data received successfully' });
 });
